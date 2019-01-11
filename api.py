ce1546d3b23d28313d058559b944d65ab0fa4381
Vishay Patel <vishay28@gmail.com>
2019-01-11 18:40:01 +0000

    Fix patch valve

--- api.py
+++ api.py
@@ -43,13 +43,13 @@ def pourDrink(alcohol, mixer, shots):
     GPIO.output(mixerPin, GPIO.HIGH)
 
 def updateValveData(valve, valvePin, valveDrink, valveType):
-    if (valvePin !== ''):
+    if (valvePin and valvePin != "''"):
         db.execute("UPDATE valves SET valve_pin = " + valvePin + " WHERE valve_number = " + str(valve))
         dbConnection.commit()
-    if (valveDrink !== ''):
+    if (valveDrink and valveDrink != "''"):
         db.execute("UPDATE valves SET valve_drink = " + valveDrink + " WHERE valve_number = " + str(valve))
         dbConnection.commit()
-    if (valveType !== ''):
+    if (valveType and valveType != "''"):
         db.execute("UPDATE valves SET valve_type = " + valveType + " WHERE valve_number = " + str(valve))
         dbConnection.commit()
 
